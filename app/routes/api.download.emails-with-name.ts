952d1ed5cc04a9632c7814ef79b31c70bc9d0a8d
Sami <srmf2025@gmail.com>
2024-01-13 19:50:42 +0530

    fix: protect download api

--- app/routes/api.download.emails-with-name.ts
+++ app/routes/api.download.emails-with-name.ts
@@ -1,7 +1,11 @@
 import { convertToCSV } from "~/utils/convert.server";
 import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
+import type { LoaderFunction } from "@remix-run/node";
 
-export const loader = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+
   const emails = await db.email.findMany({
     where: {
       name: {
